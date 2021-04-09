e725e8d760de6ca5ae75dde204420ab3c44958ef
nohsh92 <nohsh92@gmail.com>
2021-04-09 19:43:31 +0900

    Finished with picture upload code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,6 +22,8 @@ class MyApp extends StatelessWidget {
       LandingScreen.id: (context) => LandingScreen(),
       LoginSection.id: (context) => LoginSection(),
       LogoutScreen.id: (context) => LogoutScreen(),
+      ImageCapture.id: (context) => ImageCapture(),
+      Uploader.id: (context) => Uploader(),
     });
   }
 }
@@ -210,6 +217,12 @@ class LandingScreen extends StatelessWidget {
           Center(child: Text("Welcome to the Landing Screen")),
           TextButton.icon(
               onPressed: () async {
+                Navigator.pushNamed(context, ImageCapture.id);
+              },
+              icon: Icon(Icons.arrow_forward),
+              label: Text('Onto Pics')),
+          TextButton.icon(
+              onPressed: () async {
                 SharedPreferences prefs = await SharedPreferences.getInstance();
                 await prefs.setString('token', '');
                 Navigator.pushNamed(context, LoginSection.id);
@@ -294,5 +307,124 @@ class LogoutScreen extends StatelessWidget {
       ),
       bottomNavigationBar: _BottomNav(),
     );
+  }
+}
+
+class ImageCapture extends StatefulWidget {
+  static const String id = "ImageCapture";
+  @override
+  _ImageCaptureState createState() => _ImageCaptureState();
+}
+
+class _ImageCaptureState extends State<ImageCapture> {
+  File _imageFile;
+
+  Future<void> _pickImage(ImageSource source) async {
+    File selected = await ImagePicker.pickImage(source: source);
+
+    setState(() {
+      _imageFile = selected;
+    });
+  }
+
+  Future<void> _cropImage() async {
+    File cropped = await ImageCropper.cropImage(
+        sourcePath: _imageFile.path,
+        toolbarColor: Colors.lightBlue,
+        toolbarWidgetColor: Colors.white,
+        toolbarTitle: 'Crop');
+
+    setState(() {
+      _imageFile = cropped ?? _imageFile;
+    });
+  }
+
+  void _clear() {
+    setState(() => _imageFile = null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.photo_camera),
+              onPressed: () => _pickImage(ImageSource.camera),
+            ),
+            IconButton(
+              icon: Icon(Icons.photo_library),
+              onPressed: () => _pickImage(ImageSource.gallery),
+            ),
+            if (_imageFile != null) Image.file(_imageFile),
+            Row(
+              children: <Widget>[
+                TextButton(
+                  child: Icon(Icons.crop),
+                  onPressed: _cropImage,
+                ),
+              ],
+            ),
+            Uploader(file: _imageFile)
+          ]),
+      bottomNavigationBar: _BottomNav(),
+    );
+  }
+}
+
+class Uploader extends StatefulWidget {
+  static const String id = "Uploader";
+  final File file;
+
+  Uploader({Key key, this.file}) : super(key: key);
+
+  createState() => _UploaderState();
+}
+
+class _UploaderState extends State<Uploader> {
+  final FirebaseStorage _storage =
+      FirebaseStorage(storageBucket: 'gs://findit-22575.appspot.com');
+
+  StorageUploadTask _uploadTask;
+
+  void _startUpload() {
+    String filePath = 'images/${DateTime.now()}.png';
+
+    setState(() {
+      _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_uploadTask != null) {
+      return StreamBuilder<StorageTaskEvent>(
+          stream: _uploadTask.events,
+          builder: (context, snapshot) {
+            var event = snapshot?.data?.snapshot;
+
+            double progressPercent = event != null
+                ? event.bytesTransferred / event.totalByteCount
+                : 0;
+
+            return Column(
+              children: [
+                if (_uploadTask.isComplete) Text('Congrats'),
+                if (_uploadTask.isPaused)
+                  TextButton(
+                      onPressed: _uploadTask.pause, child: Icon(Icons.pause)),
+                LinearProgressIndicator(value: progressPercent),
+                Text('${(progressPercent * 100).toStringAsFixed(2)} % ')
+              ],
+            );
+          });
+    } else {
+      return TextButton.icon(
+        onPressed: () => _startUpload(),
+        label: Text('Upload to Firebase'),
+        icon: Icon(Icons.cloud_upload),
+      );
+    }
   }
 }
